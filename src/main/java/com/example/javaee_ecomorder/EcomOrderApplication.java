effa6ae25e9c139cc14c029a7e26844844c12b3c
ddd965 <2958821877@qq.com>
2026-05-15 13:51:24 +0800

    添加启动类EcomOrderApplication

--- src/main/java/com/example/javaee_ecomorder/EcomOrderApplication.java
+++ src/main/java/com/example/javaee_ecomorder/EcomOrderApplication.java
@@ -1,13 +1,13 @@
 package com.example.javaee_ecomorder;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.javaee_ecomorder.mapper")  // 扫描所有 Mapper 接口
 public class EcomOrderApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EcomOrderApplication.class, args);
     }
-
 }
